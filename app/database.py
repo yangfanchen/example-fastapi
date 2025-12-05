81ed88e1c011fdcb0fbc9e87cbc4a486b0408b8c
yangfanchen <54922665+yangfanchen@users.noreply.github.com>
2025-12-04 21:19:14 -0400

    remove the unused connect and updated database url

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 #SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@<ip-address>/hostname/<database_name>'
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:123456@localhost/fastapi'
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL ="postgresql://admin:xhy3ajG8gGRqRjkgy6pr6wQTHmUc5zqZ@dpg-d4ovouvgi27c73edo9og-a.oregon-postgres.render.com/fastapi_11ap"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
@@ -20,15 +20,3 @@ def get_db():
         db.close()
 
 
-while True:
-
-    try:
-        conn = psycopg2.connect(host='localhost',database='fastapi',
-                                user='postgres',password='123456')
-        cursor = conn.cursor()
-        print("database connection was successful")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ",error)
-        time.sleep(2)
