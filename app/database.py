4179b17f4293956ab0cb2506e5cf0e706ead4710
yangfanchen <54922665+yangfanchen@users.noreply.github.com>
2025-12-06 22:04:58 -0400

    uses static database db

--- app/database.py
+++ app/database.py
@@ -8,9 +8,9 @@ from .config import settings
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:123456@localhost/fastapi'
 #SQLALCHEMY_DATABASE_URL ="postgresql://admin:xhy3ajG8gGRqRjkgy6pr6wQTHmUc5zqZ@dpg-d4ovouvgi27c73edo9og-a.oregon-postgres.render.com/fastapi_11ap"
 #postgresql://admin:jjOEwz9xHW1HGol4WOmi2W93ngTBedIR@dpg-d4qdbebe5dus73elnni0-a.virginia-postgres.render.com/fastapi_o5ta
-SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+#SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-#SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password123@postgres:5432/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:jjOEwz9xHW1HGol4WOmi2W93ngTBedIR@dpg-d4qdbebe5dus73elnni0-a.virginia-postgres.render.com/fastapi_o5ta"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
