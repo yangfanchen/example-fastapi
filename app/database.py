1e673999d246107991cfcd586dde429d22024b6d
yangfanchen <54922665+yangfanchen@users.noreply.github.com>
2025-12-06 21:58:59 -0400

    added database

--- app/database.py
+++ app/database.py
@@ -7,8 +7,8 @@ from .config import settings
 #SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@<ip-address>/hostname/<database_name>'
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:123456@localhost/fastapi'
 #SQLALCHEMY_DATABASE_URL ="postgresql://admin:xhy3ajG8gGRqRjkgy6pr6wQTHmUc5zqZ@dpg-d4ovouvgi27c73edo9og-a.oregon-postgres.render.com/fastapi_11ap"
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+#postgresql://admin:jjOEwz9xHW1HGol4WOmi2W93ngTBedIR@dpg-d4qdbebe5dus73elnni0-a.virginia-postgres.render.com/fastapi_o5ta
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 #SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password123@postgres:5432/fastapi"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
